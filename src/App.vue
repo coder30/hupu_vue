e1f40f265e0d3a16f43bbecf9448d9d6c43983fb
curry <curry@currydeMacBook-Pro.local>
2019-07-21 09:53:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item>
+        <img
+          slot="icon"
+          slot-scope="props"
+          :src="props.active ? icon.home_active : icon.home_normal"
+        >
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <img
+          slot="icon"
+          slot-scope="props"
+          :src="props.active ? icon.ball_acitve : icon.ball_normal"
+        >
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <img
+          slot="icon"
+          slot-scope="props"
+          :src="props.active ? icon.forum_active : icon.forum_normal"
+        >
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Tabbar, TabbarItem } from 'vant'
+Vue.use(Tabbar).use(TabbarItem)
+import {icon} from './constant/image'
+export default {
+  name: 'app',
+  data() {
+    return {
+      active: 0,
+      icon
+    }
+  }
+}
+</script>
+
+<style>
+</style>
